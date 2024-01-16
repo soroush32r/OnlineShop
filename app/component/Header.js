1dc5f364b24fa904c0080da3db0e8104daebac39
Soroush <ranjbar.soroush32@gmail.com>
2024-01-16 15:55:33 +0330

    Make Header Responsive

--- app/component/Header.js
+++ app/component/Header.js
@@ -1,36 +1,36 @@
-'use client'
-import Link from "next/link"
-import { useSelector } from "react-redux"
-import Search from "./Search"
-
+"use client";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import Search from "./Search";
 
 const Header = () => {
-  const {cart} = useSelector(store => store.cart)
-  const cartCount = cart.reduce(( pre, cur ) => pre = pre + cur.count, 0)
-  
+  const { cart } = useSelector((store) => store.cart);
+  const cartCount = cart.reduce((pre, cur) => (pre = pre + cur.count), 0);
+
   return (
-    <div className="bg-blue-400 text-white flex justify-between items-center w-full opacity-90 py-3 px-5 sticky top-0 right-0">
-      <Link href={'/'}>
-        <div>
-            Home
-        </div>
+    <div className="bg-blue-400 text-white flex justify-between items-center w-screen opacity-90 py-3 px-5 sticky top-0 right-0">
+      <Link href={"/"}>
+        <div>Home</div>
       </Link>
       <Search />
-      <Link href={'/cart'} >
-        <div className=" px-2 flex cursor-pointer"> 
-          Cart {cartCount ? 
-          <div className="flex">
-            ({cartCount})
-            <span className="relative flex h-3 w-3">
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-600 opacity-75"></span>
-              <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
-            </span>
-          </div>  : ''
-        }
+      <Link href={"/cart"}>
+        <div className=" md:px-2 flex cursor-pointer">
+          Cart{" "}
+          {cartCount ? (
+            <div className="flex">
+              ({cartCount})
+              <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-600 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- app/component/Search.js
+++ app/component/Search.js
@@ -1,34 +1,41 @@
-'use client'
-import { useCallback, useMemo, useState } from "react"
-import { searchByName } from "../utils/searchByName"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import { useCallback, useMemo, useState } from "react";
+import { searchByName } from "../utils/searchByName";
+import Image from "next/image";
+import Link from "next/link";
 
 const Search = () => {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   const handleChange = useCallback((e) => {
-    setSearch(e.target.value)
-  }, [])
+    setSearch(e.target.value);
+  }, []);
 
-  const SEARCH_PRODUCT = useMemo(() => searchByName(search), [search])
+  const SEARCH_PRODUCT = useMemo(() => searchByName(search), [search]);
 
   return (
-    <div className="text-black relative">
-      <input placeholder="search" value={search} onChange={handleChange} className="border-none rounded px-2 py-1"/>
+    <div className="text-black relative w-2/3 md:w-5/6 mx-2">
+      <input
+        placeholder="search"
+        value={search}
+        onChange={handleChange}
+        className="border-none rounded px-2 py-1 w-full"
+      />
       <div className="w-full absolute shadow-md">
-        {!!search && SEARCH_PRODUCT.map(product => (
-          <Link href={`/${product.category}/${product.name}`} onClick={() => setSearch('')}>
+        {!!search &&
+          SEARCH_PRODUCT.map((product) => (
+            <Link
+              href={`/${product.category}/${product.name}`}
+              onClick={() => setSearch("")}
+            >
               <div className="flex bg-gray-100 ">
-                <Image src={product.image} alt={product.name} width={30}  />
-                <div>
-                  {product.name}
-                </div>
+                <Image src={product.image} alt={product.name} width={30} />
+                <div>{product.name}</div>
               </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
